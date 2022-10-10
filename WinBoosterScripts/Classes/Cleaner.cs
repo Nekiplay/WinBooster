6c7702c29c0e40da7bc85683443f275f173016dc
Рома Данилов <neci.03@mail.ru>
2022-10-10 08:07:06 +0500

    More lua access

--- WinBoosterScripts/Classes/Cleaner.cs
+++ WinBoosterScripts/Classes/Cleaner.cs
@@ -7,6 +7,9 @@ namespace WinBoosterScripts.Classes
     {
         public LogsManager Logs = new LogsManager();
         public CacheManager Cache = new CacheManager();
+        public ImagesManager Images = new ImagesManager();
+        public MediaManager Media = new MediaManager();
+        public CheatsManager Cheats = new CheatsManager();
         public class LogsManager
         {
             public void Add(string directory)
@@ -41,6 +44,60 @@ namespace WinBoosterScripts.Classes
                 get
                 {
                     return Files.cache.Count;
+                }
+            }
+        }
+        public class ImagesManager
+        {
+            public void Add(string directory)
+            {
+                Files.images.Add(new DirectoryFull(directory));
+            }
+            public void Add(string directory, string patern)
+            {
+                Files.images.Add(new DirectoryPatern(directory, patern));
+            }
+            public int Count
+            {
+                get
+                {
+                    return Files.images.Count;
+                }
+            }
+        }
+        public class MediaManager
+        {
+            public void Add(string directory)
+            {
+                Files.media.Add(new DirectoryFull(directory));
+            }
+            public void Add(string directory, string patern)
+            {
+                Files.media.Add(new DirectoryPatern(directory, patern));
+            }
+            public int Count
+            {
+                get
+                {
+                    return Files.media.Count;
+                }
+            }
+        }
+        public class CheatsManager
+        {
+            public void Add(string directory)
+            {
+                Files.cheats.Add(new DirectoryFull(directory));
+            }
+            public void Add(string directory, string patern)
+            {
+                Files.cheats.Add(new DirectoryPatern(directory, patern));
+            }
+            public int Count
+            {
+                get
+                {
+                    return Files.cheats.Count;
                 }
             }
         }
